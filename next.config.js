1369bc79ef1d42e50eecfa29984efca18ec1b92f
Mercury-nju <2945235656@qq.com>
2025-09-28 15:29:58 +0800

    Disable webpack cache to fix Cloudflare Pages 25MB limit

--- next.config.js
+++ next.config.js
@@ -3,6 +3,17 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
+  // 禁用构建缓存
+  experimental: {
+    webpackBuildWorker: false,
+  },
+  // 配置 webpack 禁用缓存
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.cache = false;
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
