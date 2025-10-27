4bc2c145383af14c7563e9c9115ebfaf6b0cd461
Mercury-nju <2945235656@qq.com>
2025-10-27 19:58:24 +0800

    Fix gray borders by removing flex from main wrapper, use ml instead of pl for sidebar offset

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -169,7 +169,7 @@ export default function DashboardLayout({
   }
 
   return (
-    <div className="min-h-screen bg-white lg:flex">
+    <div className="min-h-screen bg-white">
       {/* Mobile sidebar */}
       <div className={`fixed inset-0 z-50 lg:hidden ${sidebarOpen ? 'block' : 'hidden'}`}>
         <div className="fixed inset-0 bg-gray-600 bg-opacity-75" onClick={() => setSidebarOpen(false)} />
@@ -269,7 +269,7 @@ export default function DashboardLayout({
       </div>
 
       {/* Main content */}
-      <div className="flex-1 min-w-0 lg:pl-64">
+      <div className="lg:ml-64">
         {children}
       </div>
 
